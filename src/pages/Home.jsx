7da1517c8710e5846cf1ade0a519ee87cc334bf4
Meghakumari2 <megha.kumari@marketplug.com>
2026-04-26 19:14:11 +0500

    added linkedIn summary in the project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,14 @@ import '../styles/main.css'
 import { downloadDocx } from "../utils/downloadDocs";
 export default function Home() {
   const [jobDescription, setJobDescription] = useState("");
+  const [linkedIn_summary, setLinkedInSummary] = useState("");
   const [result, setResult] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleGenerate = async () => {
     setLoading(true);
     try {
-      const data = await generateCoverLetter(jobDescription);
+      const data = await generateCoverLetter(jobDescription, linkedIn_summary);
       setResult(data.cover_letter);
     } catch (err) {
       console.error(err);
@@ -22,11 +23,21 @@ return (
   <div className="container">
     <h1>ApplyAI</h1>
 
+<p className="description">
+  Paste the job description and your LinkedIn summary to generate a professional cover letter based on your real experience.
+</p>
+
     <div className="card">
       <textarea
         placeholder="Paste job description here..."
         value={jobDescription}
         onChange={(e) => setJobDescription(e.target.value)}
+      />
+    <br />
+    <br />
+      <textarea type="text" placeholder="LinkedIn profile Summary" 
+       value={linkedIn_summary}
+       onChange={(e) => setLinkedInSummary(e.target.value)}
       />
 
       <button onClick={handleGenerate} disabled={loading}>
@@ -44,8 +55,6 @@ return (
         </button>
         </div>
         <p style={{ whiteSpace: "pre-line" }}>{result}</p>
-
-         
       </div>
     )}
   </div>
--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,12 @@
-export const generateCoverLetter = async (jobDescription) => {
+export const generateCoverLetter = async (jobDescription, linkedIn_summary) => {
   const response = await fetch("https://meghadev.app.n8n.cloud/webhook/9ac1e28e-1182-43d3-a8b8-880ab37d41a7", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      job_description: jobDescription
+      job_description: jobDescription,
+      linkedin_summary: linkedIn_summary
     })
   });
 
